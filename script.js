9579bc80b182305fd31afec223c9253417fc3ebd
Kevin Kurniadi <kurniadikevin@gmail.com>
2023-01-28 14:44:12 +0700

    add minor

--- script.js
+++ script.js
@@ -25,33 +25,63 @@ const chord = function (roots,type){
     return arrResult;
   }
 
+  //minor
   else if(type === 'minor'){
     let root = roots.toUpperCase();
     
     let rootIndex= notes.indexOf(root);
       
+    //third index
     let thirdIndex;
-        if(rootIndex+4 > 11){
+    //octave note
+    if(rootIndex+4 > 11){
       thirdIndex = notes[(rootIndex + 4) - notes.length];
-    } else{
-      thirdIndex= notes[rootIndex+4] + 'b'
+
+      // third node sharp on mayor
+      if(thirdIndex.length===2){
+        thirdIndex = notes[(rootIndex + 3) - notes.length];
+      }
+
+    } 
+
+    //non-octave
+    else{
+      thirdIndex= notes[rootIndex+4] + 'b';// third node natural note
     }
-      
+
+    const naturalThirdIndex= function(item){
+        if(item ==='Fb'){
+            return 'E';
+        } else if( item === 'Cb'){
+            return 'B';
+        }else if(item.length===3){
+            let itemArr = item.split('');
+            return itemArr[0];
+        } 
+        else{
+            return item;
+        }
+
+    }
+
+    //fifth index
     let fifthIndex;
       if(rootIndex+7 > 11){
       fifthIndex = (rootIndex + 7) - notes.length;
     } else{
       fifthIndex= rootIndex+7;
     }
-      
-    let arrResult = [notes[rootIndex],thirdIndex ,notes[fifthIndex]]
+
+    let arrResult = [notes[rootIndex],naturalThirdIndex(thirdIndex) ,notes[fifthIndex]]
       return arrResult;
   }
 }
 
 console.log(
-chord('b','mayor')
+chord('c','mayor')
   );
+
+
 
 
 
