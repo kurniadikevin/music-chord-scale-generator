80912d95e7de553ca06ab9eef5228352780a9c99
Kevin Kurniadi <kurniadikevin@gmail.com>
2023-01-30 21:32:27 +0700

    add seventh chord

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const notes =['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
 
+//substitution
+const notesNaturally =['C','C#','D','Eb','E','F','F#','G','Ab','A','Bb','B'];
+
 const chord = function (roots,type){
-  //mayor
+
+  //MAYOR
   if(type ==='mayor'){
-  let root = roots.toUpperCase();
-    
+  let root = roots.toUpperCase();  
   let rootIndex= notes.indexOf(root);
-    
+  
+  //third mayor
   let thirdIndex;
       if((rootIndex+4) > 11){
     thirdIndex = (rootIndex + 4) - notes.length;
@@ -25,34 +29,19 @@ const chord = function (roots,type){
     return arrResult.join(' ');
   }
 
-  //minor
+  //MINOR
   else if(type === 'minor'){
     let root = roots.toUpperCase();
     
     let rootIndex= notes.indexOf(root);
       
-    //third index
+    //third minor
     let thirdIndex;
     if((rootIndex+3) > 11){
-     thirdIndex = notes[(rootIndex + 3) - notes.length];
+     thirdIndex = (rootIndex + 3) - notes.length;
     } else{
-     thirdIndex= notes[rootIndex+3];
+     thirdIndex= rootIndex+3;
     }
-
-  /*   const naturalThirdIndex= function(item){
-        if(item ==='Fb'){
-            return 'E';
-        } else if( item === 'Cb'){
-            return 'B';
-        }else if(item.length===3){
-            let itemArr = item.split('');
-            return itemArr[0];
-        } 
-        else{
-            return item;
-        }
-
-    } */
 
     //fifth index
     let fifthIndex;
@@ -62,16 +51,121 @@ const chord = function (roots,type){
       fifthIndex= rootIndex+7;
     }
 
-    let arrResult = [notes[rootIndex],thirdIndex ,notes[fifthIndex]]
+    let arrResult = [notes[rootIndex],notes[thirdIndex] ,notes[fifthIndex]]
       return arrResult.join(' ');
   }
+
+  //MAYOR7
+  else if(type === 'mayor7'){
+    let root = roots.toUpperCase(); 
+    let rootIndex= notes.indexOf(root);
+      
+    //third mayor
+    let thirdIndex;
+        if((rootIndex+4) > 11){
+      thirdIndex = (rootIndex + 4) - notes.length;
+    } else{
+      thirdIndex= rootIndex+4;
+    }
+      
+    let fifthIndex;
+      if(rootIndex+7 > 11){
+      fifthIndex = (rootIndex + 7) - notes.length;
+    } else{
+      fifthIndex= rootIndex+7;
+    }
+    
+    //seventh mayor
+    let seventhIndex;
+    if(rootIndex+11 > 11){
+      seventhIndex = (rootIndex + 11) - notes.length;
+    } else{
+      seventhIndex= rootIndex+11;
+    }
+
+    let arrResult = [notes[rootIndex],notes[thirdIndex],notes[fifthIndex],notes[seventhIndex]];
+      return arrResult.join(' ');
+    }
+
+    //MINOR7
+    else if(type === 'minor7'){
+      let root = roots.toUpperCase();
+      
+      let rootIndex= notes.indexOf(root);
+        
+      //third index minor
+      let thirdIndex;
+      if((rootIndex+3) > 11){
+       thirdIndex = notes[(rootIndex + 3) - notes.length];
+      } else{
+       thirdIndex= notes[rootIndex+3];
+      }
+  
+      //fifth index
+      let fifthIndex;
+        if(rootIndex+7 > 11){
+        fifthIndex = (rootIndex + 7) - notes.length;
+      } else{
+        fifthIndex= rootIndex+7;
+      }
+
+      //seventh index minor
+      let seventhIndex;
+      if((rootIndex+10) > 11){
+        seventhIndex = (rootIndex + 10) - notes.length;
+       } else{
+        seventhIndex= rootIndex+10;
+       }
+  
+      let arrResult = [notes[rootIndex],thirdIndex ,notes[fifthIndex],notes[seventhIndex]];
+        return arrResult.join(' ');
+    }
+
+    //DOMINANT7
+  else if(type === 'dom7'){
+    let root = roots.toUpperCase(); 
+    let rootIndex= notes.indexOf(root);
+    
+    //mayor
+    let thirdIndex;
+        if((rootIndex+4) > 11){
+      thirdIndex = (rootIndex + 4) - notes.length;
+    } else{
+      thirdIndex= rootIndex+4;
+    }
+      
+    let fifthIndex;
+      if(rootIndex+7 > 11){
+      fifthIndex = (rootIndex + 7) - notes.length;
+    } else{
+      fifthIndex= rootIndex+7;
+    }
+    // minor 
+    let seventhIndex;
+    if(rootIndex+10 > 11){
+      seventhIndex = (rootIndex + 10) - notes.length;
+    } else{
+      seventhIndex= rootIndex+10;
+    }
+
+    let arrResult = [notes[rootIndex],notes[thirdIndex],notes[fifthIndex],notes[seventhIndex]];
+      return arrResult.join(' ');
+    }
+
 }
 
- /* console.log(
-chord('g#','mayor')
-  );  */
- 
+
+
+
+
+
+/*   console.log(
+chord('d','minor7')
+  );  
+  */
 
 export {chord};
+
+//naturalized some third exp: C minor = C Eb G not C D# G
 
 
--- test.js/chord.test.js
+++ test.js/chord.test.js
@@ -57,3 +57,88 @@ test('Returns chord minor sharp system correctly', function () {
     expect(chord('b','minor')).toBe( 'B D F#'
     );
 });
+
+test('Returns chord mayor7 sharp system correctly', function () {
+
+	expect(chord('c','mayor7')).toBe( 'C E G B'
+    );
+    expect(chord('c#','mayor7')).toBe( 'C# F G# C'
+    );
+    expect(chord('d','mayor7')).toBe( 'D F# A C#'
+    );
+    expect(chord('d#','mayor7')).toBe( 'D# G A# D'
+    );
+    expect(chord('e','mayor7')).toBe( 'E G# B D#'
+    );
+    expect(chord('f','mayor7')).toBe( 'F A C E'
+    );
+    expect(chord('f#','mayor7')).toBe( 'F# A# C# F'
+    );
+    expect(chord('g','mayor7')).toBe( 'G B D F#'
+    );
+    expect(chord('g#','mayor7')).toBe( 'G# C D# G'
+    );
+    expect(chord('a','mayor7')).toBe( 'A C# E G#'
+    );
+    expect(chord('a#','mayor7')).toBe( 'A# D F A'
+    );
+    expect(chord('b','mayor7')).toBe( 'B D# F# A#'
+    );
+});
+
+test('Returns chord minor7 sharp system correctly', function () {
+
+	
+	expect(chord('c','minor7')).toBe( 'C D# G A#'
+    );
+    expect(chord('c#','minor7')).toBe( 'C# E G# B'
+    );
+    expect(chord('d','minor7')).toBe( 'D F A C'
+    );
+     expect(chord('d#','minor7')).toBe( 'D# F# A# C#'
+    ); 
+    expect(chord('e','minor7')).toBe( 'E G B D'
+    );
+    expect(chord('f','minor7')).toBe( 'F G# C D#'
+    );
+    expect(chord('f#','minor7')).toBe( 'F# A C# E'
+    );
+    expect(chord('g','minor7')).toBe( 'G A# D F'
+    );
+      expect(chord('g#','minor7')).toBe( 'G# B D# F#'
+    );  
+    expect(chord('a','minor7')).toBe( 'A C E G'
+    );
+      expect(chord('a#','minor7')).toBe( 'A# C# F G#'
+    );  
+    expect(chord('b','minor7')).toBe( 'B D F# A'
+    );
+});
+
+test('Returns chord dominant7 sharp system correctly', function () {
+
+	expect(chord('c','dom7')).toBe( 'C E G A#'
+    );
+    expect(chord('c#','dom7')).toBe( 'C# F G# B'
+    );
+    expect(chord('d','dom7')).toBe( 'D F# A C'
+    );
+    expect(chord('d#','dom7')).toBe( 'D# G A# C#'
+    );
+    expect(chord('e','dom7')).toBe( 'E G# B D'
+    );
+    expect(chord('f','dom7')).toBe( 'F A C D#'
+    );
+    expect(chord('f#','dom7')).toBe( 'F# A# C# E'
+    );
+    expect(chord('g','dom7')).toBe( 'G B D F'
+    );
+    expect(chord('g#','dom7')).toBe( 'G# C D# F#'
+    );
+    expect(chord('a','dom7')).toBe( 'A C# E G'
+    );
+    expect(chord('a#','dom7')).toBe( 'A# D F G#'
+    );
+    expect(chord('b','dom7')).toBe( 'B D# F# A'
+    );
+});
